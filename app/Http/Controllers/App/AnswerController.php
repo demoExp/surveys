7eefc46be1bdeae12250440bc1faffed2e85f00e
i3obek <kami0507@o2.pl>
2022-06-29 23:10:39 +0200

    init + first draft of app

---
+++ app/Http/Controllers/App/AnswerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\App;
+
+use App\Http\Controllers\Controller;
+use App\Models;
+use Illuminate\Http\Request;
+
+class AnswerController extends Controller
+{
+    public function store(Models\Answer $answer, Models\Survey $survey)
+    {
+        $data = \request()->validate([
+            'answers.*.answer'  => 'required',
+            'answers.*.question_id'  => 'required',
+            'reply_amount'      => 'required',
+        ]);
+
+        $answer->insert(array_values($data['answers']));
+        $survey->update(['reply_amount' => $data['reply_amount']]);
+
+        return back();
+    }
+}
